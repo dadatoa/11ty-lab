a0ad1b83f987818abdf7284a432ca85b9d076e93
Dadatoa <dadatoa.webdev@outlook.fr>
2023-06-06 17:20:48 +0700

    templates nunjuck

--- .eleventy.js
+++ .eleventy.js
@@ -8,15 +8,24 @@ module.exports = function(eleventyConfig) {
   return {
     dir: {
 <<<<<<< HEAD
+<<<<<<< HEAD
       input: "src"
 =======
+=======
+>>>>>>> 1464752 (templates nunjuck)
       input: "src",
 <<<<<<< Updated upstream
       output: "public"
 =======
       layouts: "_layouts"
 >>>>>>> Stashed changes
+<<<<<<< HEAD
 >>>>>>> 581fcaa (ajoute layout nunchucks)
+=======
+=======
+      input: "src"
+>>>>>>> 9c0887b (templates nunjuck)
+>>>>>>> 1464752 (templates nunjuck)
     }
   }
 };
