f076331d772126efd5312b18902b1dcc165eb2e3
Dadatoa <dadatoa.webdev@outlook.fr>
2023-06-06 13:53:24 +0700

    correc congig + ajout plugins webc/render

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,14 @@
-module.export = function(eleventyConfig){
+const pluginWebc = require("@11ty/eleventy-plugin-webc");
+const { EleventyRenderPlugin } = require("@11ty/eleventy");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(pluginWebc);
+  eleventyConfig.addPlugin(EleventyRenderPlugin);
+
   return {
     dir: {
       input: "src",
       output: "public"
     }
-  };
-}
+  }
+};
